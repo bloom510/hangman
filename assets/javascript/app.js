6a6d5939a011dd11895992c7c3314c20e2050fb4
wildcat510 <bloomjazzer@gmail.com>
2017-11-08 13:59:42 -0800

    Correctly loads WebGL Ripples API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,21 @@
 // Hide anything necessary with jQuery
-$(document).ready(function hideGuess() {
+$(document).ready(function () {
   $(".letters").hide();
-  //Loads background with jQuery to smoothly reload it
-  //after game dynamically generates divs, thus avoiding formatting errors
-  // encountered previously with our flex box container
+  /*Loads background with jQuery to smoothly reload it
+    after game dynamically generates divs, thus avoiding formatting errors
+    encountered previously with our flex box container*/
   $(".container").css({"background-image": "url('https://wildcat510.github.io/images/zenkoan1.png')", "background-size": "cover", "background-repeat": "no-repeat"});
+  $('.container').ripples();
 });
 
 //Get empty divs to give data to
 var userGuess = document.getElementById("user-guess");
 var wordStage = document.getElementById("stage");
-var remainingGuesses = document.getElementById("remaining-guesses");
 var guessCount = 20;
 var alreadyGuessed = [];
 var winCount = 0;
 
-var init = false;
+
 //Array to hold HTML iterations of each letter to be rendered on the page
 var wordDisplay = [];
 
@@ -26,51 +26,50 @@ var currentword = [];
 //Collection of words for game
 var dictionary = [
   "Rainfall",
-  // "Wildcat",
-  // "Mythic",
-  // "Crystal",
-  // "Fragrant",
-  // "Petrichor",
-  // "Rain",
-  // "Winter",
-  // "Freestyle",
-  // "Gothic",
-  // "Modulus"
+  "Wildcat",
+  "Mythic",
+  "Crystal",
+  "Fragrant",
+  "Petrichor",
+  "Rain",
+  "Winter",
+  "Freestyle",
+  "Gothic",
+  "Modulus"
 ];
 
 //Stores a random word from our dictionary object inside a variable
 var randWord = dictionary[Math.floor(Math.random() * dictionary.length)];
 // console.log(init);
 
-
-
-
-//stores if the game has been initialized or not.
-wordGen.called = 0;
+/*if letter has already been guessed, do not log to wins. do nothing!
+if number of wins is equal to number of letters in word, display win alert!*/
 
 // Logs user's letter guess and updates it to userText
 document.onkeyup = function keyDetect(event) {
   guessCount--;
 
-  //Function evaluates answer and does stuff.
-  //This function is called and executed later in keyDetect()
-  //only if the key pressed is a letter.
+  /*Function evaluates answer and does stuff.
+    This function is called and executed later in keyDetect()
+    only if the key pressed is a letter.*/
   function evalWord() {
-    //using for statements with nested ifs to evaluate the word
-
-
 
     // If array currentword contains the key pressed, then do stuff.
     // Else, do more stuff
-    // Includes method: currentword.includes(event.key.toLowerCase())
-
     if (currentword.includes(event.key.toLowerCase())) {
-
+      //reveal that letter
       $("." + event.key).show();
+      //add 1 to winCount
       winCount++;
+      //update wins
       document.getElementById("wins").innerHTML = "Wins: " + winCount;
-      remainingGuesses.innerHTML = "Remaining guesses: " + guessCount;
-      console.log(currentword + " includes the letter " + event.key);
+      //update guessCount
+      document.getElementById("remaining-guesses").innerHTML = guessCount;
+      //troubleshoot
+      console.log(currentword.join("") + " includes the letter " + event.key);
+    } else {
+      //troubleshoot
+      console.log(currentword.join("") + " does not include the letter " + event.key);
     }
 
   }
@@ -84,8 +83,9 @@ document.onkeyup = function keyDetect(event) {
     userGuess.textContent = event.key.toLowerCase();
     evalWord();
 
+    //move this outside and put in if statement for preventing guessing the same letter again
     alreadyGuessed.push(event.key);
-    document.getElementById("already-guessed").innerHTML = "Already guessed: "+ alreadyGuessed;
+    document.getElementById("already-guessed").innerHTML = alreadyGuessed;
 
   }
 }
@@ -99,20 +99,22 @@ function wordGen() {
   //1. Push HTML to array wordDisplay for HTML to be rendered on the page
 //2. Convert the randomly chosen word into individual letters and store in an array called "currentword"
   for (i = 0; i < randWord.length; i++) {
+
       //If word includes duplicate letters, create a class for it
       //Else, create with a class of letter and unique id
     wordDisplay.push("<div class='box'><div class = '"+ randWord.charAt(i).toLowerCase() +" letters' style='margin: 0 auto; text-align: center; padding-top: 30%;'>" + randWord.charAt(i) + "</div></div>");
     currentword.push(randWord.charAt(i).toLowerCase());
+    //appendChild()? createElement()?
 
   }
-
-  //Removes commas from array and pushes wordDisplay to the DOM tree
+  //Removes commas from array and pushes wordDisplay contents to the DOM tree
   wordStage.innerHTML = wordDisplay.join("");
 
   //Uses jQuery to reload background image AFTER dynamically generated divs
   //to avoid formatting problems
-  $(".container").css({"background-image": "url('https://wildcat510.github.io/images/zenkoan1.png')", "background-size": "cover", "background-repeat": "no-repeat"});
+  // $(".container").css({"background-image": "url('https://wildcat510.github.io/images/zenkoan1.png')", "background-size": "cover", "background-repeat": "no-repeat"});
 
 }
+
 
 wordGen();
